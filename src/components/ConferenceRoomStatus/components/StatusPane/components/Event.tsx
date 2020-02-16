460c71a71aba9dfef4c70e8769d99efcf4d29578
robertspari <robert.spari@gmail.com>
2020-02-16 08:32:16 +0100

    Added kinda hack-y attempt to list animation

--- src/components/ConferenceRoomStatus/components/StatusPane/components/Event.tsx
+++ src/components/ConferenceRoomStatus/components/StatusPane/components/Event.tsx
@@ -1,17 +1,20 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import moment from "moment"
 import {styled, Theme} from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import {Box} from "@material-ui/core"
 import Divider from "@material-ui/core/Divider"
+import Collapse from "@material-ui/core/Collapse"
 
 import * as EventTools from "../../../tools/EventTools"
 import {EventType} from "../../../../../api/RoomApi"
 
 type EventProps = {
-	event: EventType,
+	event: EventType | null,
 	bottomDivider: boolean,
-	isCurrent: boolean
+	isCurrent: boolean,
+	appear: boolean,
+	disappear: boolean
 }
 
 const Wrapper = styled(Box)(({theme, active}: { theme: Theme, active: number }) => ({
@@ -19,17 +22,38 @@ const Wrapper = styled(Box)(({theme, active}: { theme: Theme, active: number }) 
 	color: active ? theme.palette.error.main : "inherit"
 }))
 
-const Event = ({event, bottomDivider, isCurrent}: EventProps) => (
-	<Box>
-		<Wrapper active={isCurrent ? 1 : 0}>
-			<Box flexShrink={0}>
-				<Typography variant="subtitle2">{moment(event.start).format("HH:mm")}</Typography>
-				<Typography variant="caption">{EventTools.getFormattedDuration(event)}</Typography>
-			</Box>
-			<Box ml={2}><Typography variant="body1">{event.subject}</Typography></Box>
-		</Wrapper>
-		{bottomDivider ? <Box mt={1} mb={1}><Divider/></Box> : null}
-	</Box>
-)
+const Event = ({event, bottomDivider, isCurrent, appear, disappear}: EventProps) => {
+	const [collapsed, setCollapsed] = useState<boolean>(appear)
+	
+	useEffect(() => {
+		if (appear)
+			setCollapsed(false)
+		else if (disappear)
+			setCollapsed(true)
+	})
+	
+	return (<Collapse in={!collapsed}>
+			{
+				event ?
+					<Box>
+						<Wrapper active={isCurrent ? 1 : 0}>
+							<Box flexShrink={0}>
+								<Typography variant="subtitle2">{moment(event.start).format("HH:mm")}</Typography>
+								<Typography variant="caption">{EventTools.getFormattedDuration(event)}</Typography>
+							</Box>
+							<Box ml={2}><Typography variant="body1">{event.subject}</Typography></Box>
+						</Wrapper>
+						{bottomDivider ? <Box mt={1} mb={1}><Divider/></Box> : null}
+					</Box> :
+					<Typography variant="overline" align="center">No more events today</Typography>
+			}
+		</Collapse>
+	)
+}
+
+Event.defaultProps = {
+	appear: false,
+	disappear: false
+}
 
 export default React.memo(Event)
