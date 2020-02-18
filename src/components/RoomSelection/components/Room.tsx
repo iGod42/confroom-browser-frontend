63ffcbae2cf4e7f96392bb43807e506248784238
robertspari <robert.spari@gmail.com>
2020-02-18 21:39:19 +0100

    Room selection buttons are now huge

--- src/components/RoomSelection/components/Room.tsx
+++ src/components/RoomSelection/components/Room.tsx
@@ -2,15 +2,21 @@ import React from "react"
 import {RoomType} from "../../../api/RoomApi"
 import Button from "@material-ui/core/Button"
 import Box from "@material-ui/core/Box"
+import styled from "@material-ui/core/styles/styled"
 
 type RoomParams = {
 	room: RoomType,
 	onSelect: () => void
 }
 
+const GiganticButton = styled(Button)(({theme}) => ({
+	padding: theme.spacing(5)
+}))
+
 const Room = ({room, onSelect}: RoomParams) =>
 	<Box m={1}>
-		<Button onClick={onSelect} variant="contained" color="secondary" size="large">{room.displayName}</Button>
+		<GiganticButton onClick={onSelect} variant="contained" color="secondary"
+						size="large">{room.displayName}</GiganticButton>
 	</Box>
 
 export default Room
