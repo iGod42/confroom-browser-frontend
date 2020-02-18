af13bc94b9637af554aa056a76771e509279504d
robertspari <robert.spari@gmail.com>
2020-02-18 20:31:56 +0100

    Added api support for all-day events

--- src/api/RoomApi.ts
+++ src/api/RoomApi.ts
@@ -19,14 +19,16 @@ export type EventType = {
 	id: string,
 	subject: string,
 	start: Date,
-	end: Date
+	end: Date,
+	isAllDay: boolean
 }
 
 type ApiEventType = {
 	id: string,
 	subject: string,
 	start: Date,
-	end: Date
+	end: Date,
+	isAllDay: boolean
 }
 
 function getEvents(roomId: string, from?: Date, to?: Date): Promise<EventType[]> {
@@ -44,12 +46,26 @@ function getEvents(roomId: string, from?: Date, to?: Date): Promise<EventType[]>
 		.then(res => (res as ApiEventType[]).map(convertEvent))
 }
 
+/* All day events will be returned with UTC Date set, so we need to account for that here*/
 function convertEvent(event: ApiEventType): EventType {
-	return {
+	const convertUTCDateToLocalTime = (date: Date): Date =>
+		new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+			date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds())
+	
+	const converted = {
 		...event,
 		start: new Date(event.start),
 		end: new Date(event.end)
 	}
+	
+	if (event.isAllDay) {
+		converted.start = convertUTCDateToLocalTime(converted.start)
+		converted.end = convertUTCDateToLocalTime(converted.end)
+		// make sure that inclusive end queries don't pick up this date
+		converted.end.setMilliseconds(converted.end.getMilliseconds() - 1)
+	}
+	
+	return converted
 }
 
 export default {getRoom, getRooms, getEvents}
