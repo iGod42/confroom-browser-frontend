0ef1f8fab79f2f02d772f784a429be00c645d8b9
robertspari <robert.spari@gmail.com>
2020-02-21 09:52:05 +0100

    Added pi setup script

--- README.md
+++ README.md
@@ -6,3 +6,26 @@ Idea is that this is deployed to a RaspberryPI like device with a small display 
 - Select Rooms 
 - Display events 
 - Receive quick updates with socket.io
+
+## Installation
+###React App
+1. Make sure that you set up src/config.json to the proper values
+    1. Link to hub is especially important here
+1. Build 
+1. Host built version accessible in your network
+
+###Pi
+The idea is to be able  to run this on a Pi zero with a cheap display for minimal cost. 
+So we'll need to watch memory carefully. Therefore no window environment or stuff like that is used. 
+Just a plain XServer running a single application, chromium in kiosk mode, to access the Frontend URL 
+
+1. Install Raspian Buster **light** (only tested with Buster)
+1. Copy setupPi.sh to your home directory
+1. `chmod u+x setupPi.sh`
+1. `sudo ./setupPi.sh {YOUR_FRONTEND_URL}`
+    * Installs xorg & chromium 
+    * Makes sure some default settings for chromium to be displayed correctly on a small display are present.
+    * Sets up a user "confroom"
+    * Enables autologin / autostart for chromium
+1. Install whatever display driver you'reusing. (*It's important that this is **not** done before running the install script, as those installers usually modify XServer settings which would not be present before running setup*)
+1. Reboot
---
+++ setupPi.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+die() {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 1 ] || die "Please provide the [url] to connect to as an argument"
+
+sudo apt update
+sudo apt install -y xorg xserver-xorg-video-fbturbo chromium-browser
+
+USERNAME="confroom"
+
+# basic usercreation
+sudo useradd $USERNAME
+sudo mkdir -p /home/$USERNAME/.config/chromium/Default
+
+# startup script for chrome
+cat >/home/$USERNAME/startDisplay.sh <<EOF
+#!/bin/bash
+chromium-browser --kiosk --start-maximized --disable-restore-background-contents --disable-translate --disable-new-tab-first-run $1
+EOF
+
+chmod u+x /home/$USERNAME/startDisplay.sh
+
+# chrome browser config is necessary because for some reason chrome starts with a margin
+cat >/home/$USERNAME/.config/chromium/Default/Preferences <<EOF
+{
+    "browser": {
+        "window_placement": {
+        "bottom": 10000,
+        "left": 0,
+        "maximized": true,
+        "right": 10000,
+        "top": 0
+        }
+    }
+}
+EOF
+
+# setup xsession to launch whatever's in the startup script
+cat >/home/$USERNAME/.xsession <<EOF
+xset -dpms s off #To avoid screen going blank after a while
+/home/$USERNAME/startDisplay.sh
+EOF
+
+# autostart x when logging in
+cat >/home/$USERNAME/.bashrc <<'EOF'
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+if [[ -z $DISPLAY ]] && [[ $(tty) == "/dev/tty1" ]]; then
+    	echo intheif
+	while true; do startx -- -nocursor -depth 16; echo "Again [0]..."; done
+fi
+EOF
+
+# enable autologin
+cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $USERNAME --noclear %I \$TERM
+EOF
+
+# copy default .profile so that .bashrc is actually read
+sudo cp /etc/skel/.profile /home/$USERNAME/.
+
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME
