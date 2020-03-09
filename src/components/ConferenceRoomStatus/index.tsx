69bb2cfbc5c3bf4d741eb9344c6d2852020d7b77
Robert Spari <robert.spari@leftshift.one>
2020-03-09 16:58:13 +0100

    Book minutes button now shows available mins

--- src/components/ConferenceRoomStatus/index.tsx
+++ src/components/ConferenceRoomStatus/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 import StatusPane from "./components/StatusPane"
 import ActionButton from "./components/ActionButton"
@@ -7,37 +7,40 @@ import StatusWrapper from "./components/StatusWrapper"
 import * as EventTools from "./tools/EventTools"
 import useCurrentTime from "./hooks/useCurrentTime"
 import useRoomName from "./hooks/useRoomName"
-import useEvents, {bookRoom as bookRoomAction, finishCurrentMeeting} from "./hooks/useEvents"
+import useEvents, { bookRoom as bookRoomAction, finishCurrentMeeting } from "./hooks/useEvents"
 
 import ErrorPane from "./components/ErrorPane"
 
-const ConferenceRoomStatus = ({socketUrl}: { socketUrl: string }) => {
+const ConferenceRoomStatus = ({ socketUrl }: { socketUrl: string }) => {
 	const currentTime = useCurrentTime()
-	
-	const {roomId} = useParams()
-	
+
+	const { roomId } = useParams()
+
 	const roomName = useRoomName(roomId)
-	
+
 	const [eventsState, eventDispatch] = useEvents(roomId, currentTime, socketUrl)
-	const {events, isLoading, error} = eventsState
-	
+	const { events, isLoading, error } = eventsState
+
 	const currentEvent = EventTools.getCurrentEvent(currentTime, events)
-	
+
 	const bookRoom = () => {
 		eventDispatch(bookRoomAction(roomId || "", currentTime, 15))
 	}
-	
+
 	const stopCurrent = () => {
 		eventDispatch(finishCurrentMeeting(currentTime, roomId || ""))
 	}
-	
+
+	const nextEvent = EventTools.getTodaysFutureEvents(currentTime, events, { includeCurrent: false })[0]
+	const timeToBook = Math.ceil(Math.min((nextEvent ? (nextEvent.start.getTime() - currentTime.getTime()) / 60000 : 999), 15))
+
 	return <StatusWrapper>
-		{(!events.length && error) ? <ErrorPane message={error}/> :
+		{(!events.length && error) ? <ErrorPane message={error} /> :
 			<React.Fragment>
-				<StatusPane currentTime={currentTime} events={events} roomName={roomName}/>
+				<StatusPane currentTime={currentTime} events={events} roomName={roomName} />
 				{isLoading ? null : !currentEvent ?
 					<ActionButton onClick={bookRoom} color="secondary" variant={"extended"} size="large"><i
-						className="material-icons">meeting_room</i>Book 15 min</ActionButton>
+						className="material-icons">meeting_room</i>Book {timeToBook} min</ActionButton>
 					: <ActionButton onClick={stopCurrent} color="secondary" variant={"extended"} size="large"><i
 						className="material-icons">check</i>Meeting Done</ActionButton>}
 			</React.Fragment>}
